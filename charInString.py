25551974511f88dd366b6c250ab28f37e13f9476
tushardray <tushardray@gmail.com>
2022-03-21 18:51:50 -0400

    Add files via upload

---
+++ charInString.py
@@ -0,0 +1,7 @@
+
+def findposition():
+    string = ("The quick brown fox jumps over the lazy dog")
+    location = string.find("x")
+    print('The letter "x" is found at ' + str(location))
+
+findposition()
